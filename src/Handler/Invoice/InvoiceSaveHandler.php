2a6c9adfb50dd31084f1053d2cd960b7ecaf7939
dscheinah <github@jb.chmann.net>
2026-05-25 18:07:21 +0200

    fix: handle empty and error uploads

--- src/Handler/Invoice/InvoiceSaveHandler.php
+++ src/Handler/Invoice/InvoiceSaveHandler.php
@@ -5,6 +5,7 @@ namespace App\Handler\Invoice;
 use App\Repository\InvoiceRepository;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UploadedFileInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Sx\Message\Response\ResponseHelperInterface;
 
@@ -19,7 +20,14 @@ class InvoiceSaveHandler implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $data = (array) $request->getParsedBody();
-        if (!$this->repository->saveInvoice($data, $request->getUploadedFiles()['document'] ?? null)) {
+
+        $document = $request->getUploadedFiles()['document'] ?? null;
+        assert($document === null || $document instanceof UploadedFileInterface);
+        if ($document?->getSize() === 0) {
+            $document = null;
+        }
+
+        if (!$this->repository->saveInvoice($data, $document)) {
             return $this->helper->create(400, 'Fehlerhafte Daten');
         }
         return $this->helper->create(204);
